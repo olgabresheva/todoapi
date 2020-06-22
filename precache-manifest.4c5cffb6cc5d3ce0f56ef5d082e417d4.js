86729223049188cc6f14ecd9fe39b4ba31f47d91
olgab <olgabresheva7@gmail.com>
2020-06-21 23:36:20 -0700

    Updates

---
+++ precache-manifest.4c5cffb6cc5d3ce0f56ef5d082e417d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "967df69a268c5101964ccdc0266eaf68",
+    "url": "/todoapi/index.html"
+  },
+  {
+    "revision": "284d97732ab1a3d48d76",
+    "url": "/todoapi/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5b36b0ae01f83bc680a8",
+    "url": "/todoapi/static/js/2.b7ac8fd5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapi/static/js/2.b7ac8fd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "284d97732ab1a3d48d76",
+    "url": "/todoapi/static/js/main.deb2b8c9.chunk.js"
+  },
+  {
+    "revision": "357607635286a48be366",
+    "url": "/todoapi/static/js/runtime-main.ae529cf6.js"
+  }
+]);
